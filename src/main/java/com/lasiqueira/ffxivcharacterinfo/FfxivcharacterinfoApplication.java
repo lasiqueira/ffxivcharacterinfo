3226d46f25a08a346de01ed0f7341022c333e83a
lasiqueira <lucas.asiq@gmail.com>
2020-09-27 19:28:56 +0100

    initial commit

---
+++ src/main/java/com/lasiqueira/ffxivcharacterinfo/FfxivcharacterinfoApplication.java
@@ -0,0 +1,13 @@
+package com.lasiqueira.ffxivcharacterinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfxivcharacterinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfxivcharacterinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lasiqueira/ffxivcharacterinfo/FfxivcharacterinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lasiqueira.ffxivcharacterinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfxivcharacterinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
