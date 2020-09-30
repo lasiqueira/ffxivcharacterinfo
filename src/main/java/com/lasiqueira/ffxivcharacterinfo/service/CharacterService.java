5e7cee5f4de7e976c295d0b9bc77e2273cd69585
lasiqueira <lucas.asiq@gmail.com>
2020-09-30 20:06:58 +0100

    added getCharacterSearch method to service

--- src/main/java/com/lasiqueira/ffxivcharacterinfo/service/CharacterService.java
+++ src/main/java/com/lasiqueira/ffxivcharacterinfo/service/CharacterService.java
@@ -2,7 +2,9 @@ package com.lasiqueira.ffxivcharacterinfo.service;
 
 import com.lasiqueira.ffxivcharacterinfo.infrastructure.external.client.ApiPort;
 import com.lasiqueira.ffxivcharacterinfo.infrastructure.external.dto.character.CharacterData;
+import com.lasiqueira.ffxivcharacterinfo.infrastructure.external.dto.search.CharacterSearch;
 import com.lasiqueira.ffxivcharacterinfo.model.character.Character;
+import com.lasiqueira.ffxivcharacterinfo.model.search.Search;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -12,14 +14,21 @@ import java.io.IOException;
 public class CharacterService {
     private final ApiPort apiPort;
     private final CharacterConverter characterConverter;
+    private final SearchConverter searchConverter;
 
-    public CharacterService(ApiPort apiPort, CharacterConverter characterConverter) {
+    public CharacterService(ApiPort apiPort, CharacterConverter characterConverter, SearchConverter searchConverter) {
         this.apiPort = apiPort;
         this.characterConverter = characterConverter;
+        this.searchConverter = searchConverter;
     }
 
     public Character getCharacterData(Long id) throws IOException, ResponseStatusException {
         CharacterData characterData = apiPort.getCharacterData(id);
         return characterConverter.convert(characterData);
+    }
+
+    public Search getCharacterSearch(String name, String server, Integer page) throws IOException, ResponseStatusException {
+        CharacterSearch characterSearch = apiPort.getCharacterSearch(name, server, page);
+        return searchConverter.convert(characterSearch);
     }
 }
--- src/test/java/com/lasiqueira/ffxivcharacterinfo/service/CharacterServiceTest.java
+++ src/test/java/com/lasiqueira/ffxivcharacterinfo/service/CharacterServiceTest.java
@@ -2,8 +2,11 @@ package com.lasiqueira.ffxivcharacterinfo.service;
 
 import com.lasiqueira.ffxivcharacterinfo.infrastructure.external.client.ApiAdapter;
 import com.lasiqueira.ffxivcharacterinfo.infrastructure.external.dto.character.CharacterData;
+import com.lasiqueira.ffxivcharacterinfo.infrastructure.external.dto.search.CharacterSearch;
 import com.lasiqueira.ffxivcharacterinfo.model.character.Character;
+import com.lasiqueira.ffxivcharacterinfo.model.search.Search;
 import io.github.benas.randombeans.EnhancedRandomBuilder;
+import io.github.benas.randombeans.api.EnhancedRandom;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,19 +26,27 @@ public class CharacterServiceTest {
     @MockBean
     private CharacterConverter characterConverter;
     @MockBean
+    private SearchConverter searchConverter;
+    @MockBean
     private ApiAdapter apiAdapter;
     private CharacterData characterData;
     private Character character;
+    private  CharacterSearch characterSearch;
+    private Search search;
     private static final long CHARACTER_ID = 1234l;
 
 
     @BeforeEach
     public void setup(){
+        EnhancedRandom enhancedRandom = EnhancedRandomBuilder.aNewEnhancedRandom();
         characterConverter = Mockito.mock(CharacterConverter.class);
         apiAdapter = Mockito.mock(ApiAdapter.class);
-        characterData = EnhancedRandomBuilder.aNewEnhancedRandom().nextObject(CharacterData.class);
-        character = EnhancedRandomBuilder.aNewEnhancedRandom().nextObject(Character.class);
-        characterService = new CharacterService(apiAdapter, characterConverter);
+        characterData = enhancedRandom.nextObject(CharacterData.class);
+        character = enhancedRandom.nextObject(Character.class);
+        searchConverter = Mockito.mock(SearchConverter.class);
+        characterSearch = enhancedRandom.nextObject(CharacterSearch.class);
+        search = enhancedRandom.nextObject(Search.class);
+        characterService = new CharacterService(apiAdapter, characterConverter, searchConverter);
     }
 
     @Test
@@ -47,7 +58,7 @@ public class CharacterServiceTest {
         assertNotNull(result);
     }
     @Test
-    @DisplayName("Test getting character data with Response Status Exception exception")
+    @DisplayName("Test getting character data with Response Status exception")
     public void getCharacterDataResponseStatusExceptionTest() throws IOException {
         when(characterConverter.convert(characterData)).thenReturn(character);
         when(apiAdapter.getCharacterData(Mockito.anyLong())).thenThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "Character not found"));
@@ -62,5 +73,29 @@ public class CharacterServiceTest {
         assertThrows(IOException.class, () -> characterService.getCharacterData(CHARACTER_ID));
     }
 
+    @Test
+    @DisplayName("Test getting character search")
+    public void getCharacterSearchTest() throws IOException {
+        when(searchConverter.convert(characterSearch)).thenReturn(search);
+        when(apiAdapter.getCharacterSearch(Mockito.anyString(), Mockito.anyString(), Mockito.anyInt())).thenReturn(characterSearch);
+        Search result = characterService.getCharacterSearch(Mockito.anyString(), Mockito.anyString(), Mockito.anyInt());
+        assertNotNull(result);
+    }
+    @Test
+    @DisplayName("Test getting character search with Response Status exception")
+    public void getCharacterSearchResponseStatusExceptionTest() throws IOException {
+        when(searchConverter.convert(characterSearch)).thenReturn(search);
+        when(apiAdapter.getCharacterSearch(Mockito.anyString(), Mockito.anyString(), Mockito.anyInt()))
+                .thenThrow(new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "You must provide the mandatory parameter"));
+        assertThrows(ResponseStatusException.class, () -> characterService.getCharacterSearch(Mockito.anyString(), Mockito.anyString(), Mockito.anyInt()));
+    }
+
+    @Test
+    @DisplayName("Test getting character search with IO exception")
+    public void getCharacterSearchIOExceptionTest() throws IOException {
+        when(searchConverter.convert(characterSearch)).thenReturn(search);
+        when(apiAdapter.getCharacterSearch(Mockito.anyString(), Mockito.anyString(), Mockito.anyInt())).thenThrow(new IOException());
+        assertThrows(IOException.class, () -> characterService.getCharacterSearch(Mockito.anyString(), Mockito.anyString(), Mockito.anyInt()));
+    }
 
 }
